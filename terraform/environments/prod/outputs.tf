5991bc79cf745348b238341c5b3afd487e19cfb2
Juan Camilo Salazar <jc.salazarquintero@gmail.com>
2025-06-05 14:53:19 -0500

    Fix: terraform 11

---
+++ terraform/environments/prod/outputs.tf
@@ -0,0 +1,53 @@
+# Outputs para el entorno de producción
+output "resource_group_name" {
+  description = "Nombre del grupo de recursos"
+  value       = azurerm_resource_group.prod_rg.name
+}
+
+output "resource_group_location" {
+  description = "Ubicación del grupo de recursos"
+  value       = azurerm_resource_group.prod_rg.location
+}
+
+# Outputs del cluster AKS
+output "aks_cluster_name" {
+  description = "Nombre del cluster AKS"
+  value       = module.aks.aks_cluster_name
+}
+
+output "aks_cluster_id" {
+  description = "ID del cluster AKS"
+  value       = module.aks.aks_cluster_id
+}
+
+output "aks_fqdn" {
+  description = "FQDN del API server del cluster AKS"
+  value       = module.aks.aks_fqdn
+}
+
+output "aks_public_ip" {
+  description = "IP pública del API server del cluster AKS"
+  value       = module.aks.aks_public_ip
+}
+
+output "aks_node_resource_group" {
+  description = "Grupo de recursos de los nodos AKS"
+  value       = module.aks.aks_node_resource_group
+}
+
+# Outputs de networking
+output "vnet_id" {
+  description = "ID de la red virtual"
+  value       = module.networking.vnet_id
+}
+
+output "subnet_id" {
+  description = "ID de la subred"
+  value       = module.networking.subnet_id
+}
+
+# Comando para obtener credenciales
+output "kubectl_config_command" {
+  description = "Comando para configurar kubectl"
+  value       = "az aks get-credentials --resource-group ${azurerm_resource_group.prod_rg.name} --name ${module.aks.aks_cluster_name}"
+}
--- terraform/modules/aks/outputs.tf
+++ terraform/modules/aks/outputs.tf
@@ -1,11 +1,30 @@
 output "aks_cluster_name" {
-  value = azurerm_kubernetes_cluster.aks.name
+  description = "Nombre del cluster AKS"
+  value       = azurerm_kubernetes_cluster.aks.name
+}
+
+output "aks_cluster_id" {
+  description = "ID del cluster AKS"
+  value       = azurerm_kubernetes_cluster.aks.id
+}
+
+output "aks_fqdn" {
+  description = "FQDN del API server del cluster AKS"
+  value       = azurerm_kubernetes_cluster.aks.fqdn
+}
+
+output "aks_public_ip" {
+  description = "IP pública del API server del cluster AKS"
+  value       = azurerm_kubernetes_cluster.aks.kube_config.0.host
 }
 
 output "aks_kube_config" {
-  value = azurerm_kubernetes_cluster.aks.kube_admin_config
+  description = "Configuración de kubectl para el cluster AKS"
+  value       = azurerm_kubernetes_cluster.aks.kube_admin_config
+  sensitive   = true
 }
 
 output "aks_node_resource_group" {
-  value = azurerm_kubernetes_cluster.aks.node_resource_group
+  description = "Grupo de recursos de los nodos del cluster AKS"
+  value       = azurerm_kubernetes_cluster.aks.node_resource_group
 }
